11d48ecd8e33f667c3f8290a124b346ec89a182a
Nkosana <134708939+Nkosanasydney@users.noreply.github.com>
2026-06-07 03:02:07 +0000

    fix empty date /api route for FCC tests

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ app.get("/api", (req, res) => {
   res.json({
     unix: new Date().getTime(),
     utc: new Date().toUTCString()
-  })
-})
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
